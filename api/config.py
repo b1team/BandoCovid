8e1209b41acf819b550b070347d39d52532bd649
tungnguyentu <tungnguyentu@bizflycloud.vn>
2021-10-26 23:54:04 +0700

    added api

---
+++ api/config.py
@@ -0,0 +1,29 @@
+from typing import DefaultDict
+from environs import Env
+
+env = Env()
+env.read_env() 
+
+DEAULT_KEYS = ['lastUpdateTime',
+               'wardId',
+               'wardTitle',
+               'totalAfter14Day',
+               'totalPositive',
+               'covidF1',
+               'covidF2',
+               'covidF3',
+               'covidF4',
+               'waitForTest',
+               'totalTested',
+               'totalCured',
+               'totalToday',
+               'totalDie',
+               'totalCenter',
+               'totalHouse',
+               'isLockdown',
+               'updateTime', ]
+
+class ApiConfig:
+	api_url = env.str("API_URL", "")
+	city_id = env.str("CITY_ID", "")
+	district_fields = env.list("DISTRICT_FIELDS", DEAULT_KEYS)
